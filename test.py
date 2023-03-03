fc585ffd065d58e0346c59511fa7ec02dcfa700c
Nickmerah <nickmerah@gmail.com>
2023-03-03 10:27:21 +0100

    added filter to lambda

--- test.py
+++ test.py
@@ -7,7 +7,12 @@ a = [1, 4, 5, 10]
 b = map(lambda x: x * 2, a)
 print(list(b))
 
-'''Added another Lambda'''
+'''Added multiply to  Lambda'''
 
 mult = lambda x, y: x * y
 print(mult(4, 5))
+
+'''Added filter to  Lambda'''
+
+y = filter(lambda x: x % 2 == 0, a)
+print(list(y))
