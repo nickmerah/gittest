e5794022559993055cf70de3147002fe09c4eeeb
Nickmerah <nickmerah@gmail.com>
2023-03-03 10:23:53 +0100

    Added multiply in lambda fxn

--- test.py
+++ test.py
@@ -2,6 +2,12 @@
 add10 = lambda x: x + 10
 print(add10(3))
 
+
 a = [1, 4, 5, 10]
 b = map(lambda x: x * 2, a)
 print(list(b))
+
+'''Added another Lambda'''
+
+mult = lambda x, y: x * y
+print(mult(4, 5))
